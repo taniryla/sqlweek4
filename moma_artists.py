38f18d9e91cf3285bd7caf1efe9db62cc0d931e4
Chako <ryland0000@gmail.com>
2023-09-25 14:08:43 -0700

    moma artists parse display_name and nationality

--- moma_artists.py
+++ moma_artists.py
@@ -1,2 +1,6 @@
-SELECT jsonb_pretty(info) AS formatted_info 
-FROM moma_artists LIMIT 50;
+SELECT 
+info -> 'display_name' AS name,
+info -> 'nationality' AS nationality
+FROM moma_artists 
+ORDER BY id
+LIMIT 50;
