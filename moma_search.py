3d8bff359df5953f6c1ea5d0fd15c182fef605be
Chako <ryland0000@gmail.com>
2023-09-25 12:54:34 -0700

    moma photograph query frame width and height filter

--- moma_search.py
+++ moma_search.py
@@ -1,2 +1,5 @@
-SELECT * FROM moma_works 
+SELECT
+CEIL(width) + 2 AS frame_width
+CEIL(height) + 4 AS frame_height
+FROM moma_works 
 WHERE classification = 'Photograph' AND height >0 AND width > 0;
