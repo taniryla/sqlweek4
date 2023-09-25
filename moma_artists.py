730cfa731993503d7ad2c0c6cb4f02894d5f43df
Chako <ryland0000@gmail.com>
2023-09-25 14:21:38 -0700

    moma artists insert into info json_object

--- moma_artists.py
+++ moma_artists.py
@@ -1,7 +1,8 @@
-SELECT 
-info -> 'display_name' AS name,
-info -> 'nationality' AS nationality
+INSERT INTO moma_artists (info) VALUES (
+	json_object('{display_name, Ablade Glover}', '{nationality, Ghanaian}')
+);
+
+SELECT info 
 FROM moma_artists
-WHERE info ->> 'nationality' = 'American' 
 ORDER BY id
-LIMIT 50;
+DESC LIMIT 1;
