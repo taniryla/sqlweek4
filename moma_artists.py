08da36f8d7df9b8bff5f1cdc896294b7067d32f2
Chako <ryland0000@gmail.com>
2023-09-25 14:11:06 -0700

    moma artists parse American nationality only

--- moma_artists.py
+++ moma_artists.py
@@ -1,6 +1,7 @@
 SELECT 
 info -> 'display_name' AS name,
 info -> 'nationality' AS nationality
-FROM moma_artists 
+FROM moma_artists
+WHERE info ->> 'nationality' = 'American' 
 ORDER BY id
 LIMIT 50;
