f7eaf74db1ff2e5c62d0c888e8b4739bea54d8a5
Chako <ryland0000@gmail.com>
2023-09-25 13:01:23 -0700

    moma photograph query frame area  filter

--- moma_search.py
+++ moma_search.py
@@ -1,5 +1,12 @@
+WITH frames AS (
+	SELECT
+	CEIL(width) + 2 AS frame_width,
+	CEIL(height) + 4 AS frame_height
+	FROM moma_works 
+	WHERE classification = 'Photograph' AND height >0 AND width > 0
+)
 SELECT
-CEIL(width) + 2 AS frame_width
-CEIL(height) + 4 AS frame_height
-FROM moma_works 
-WHERE classification = 'Photograph' AND height >0 AND width > 0;
+frame_width,
+frame_height,
+frame_width * frame_height AS frame_area
+FROM frames;
