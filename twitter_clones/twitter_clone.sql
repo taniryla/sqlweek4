34a8fa2424b505e293fbc7d10465d80289979853
Chako <ryland0000@gmail.com>
2023-09-27 19:53:58 -0700

    fix typos in tables and add in values

--- twitter_clones/twitter_clone.sql
+++ twitter_clones/twitter_clone.sql
@@ -1,11 +1,12 @@
 CREATE TABLE users (
 	id SERIAL PRIMARY KEY,
-	app_usersname TEXT NOT NULL UNIQUE,
+	username TEXT NOT NULL UNIQUE,
 	email TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL UNIQUE,
 	bio TEXT,
+    dob DATE,
 	profile_img TEXT,
-	bg_image TEXT
+	bg_img TEXT
 )
 
 CREATE TABLE comments (
@@ -102,7 +103,11 @@ REFERENCES posts;
 
 -- add in values
 
-INSERT INTO users (id, username, email, password, bio, dob, profile_img, bg_image)
-VALUES (1234, 'chako', 'chakoarai7@gmail.com', 'password1', 'Chako lives life better emotionally today than yesterday', 1978-2-27 07:26:49.219717, 'https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/262038495_106681035184517_3189048512221112770_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=jYuUJoTLhYwAX_gxM1S&_nc_ht=scontent-sjc3-1.xx&oh=00_AfBm3j-5jA47HS4VhUM07JBZns9-Vnm7mk81fJaze1-HJQ&oe=65194048', 'https://i.guim.co.uk/img/media/bdf23d7b1bbb200cf72ae0c22f051bf926445b20/1_0_5075_3046/master/5075.jpg?width=1900&dpr=1&s=none');
+INSERT INTO users (id, username, email, password, bio, dob, profile_img, bg_img)
+VALUES (1234, 'chako', 'chakoarai7@gmail.com', 'password1', 'Chako lives life better emotionally today than yesterday', '1978-2-27', 'https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/262038495_106681035184517_3189048512221112770_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=a2f6c7&_nc_ohc=jYuUJoTLhYwAX_gxM1S&_nc_ht=scontent-sjc3-1.xx&oh=00_AfBm3j-5jA47HS4VhUM07JBZns9-Vnm7mk81fJaze1-HJQ&oe=65194048', 'https://i.guim.co.uk/img/media/bdf23d7b1bbb200cf72ae0c22f051bf926445b20/1_0_5075_3046/master/5075.jpg?width=1900&dpr=1&s=none');
 
+INSERT INTO comments (id, post_id, user_id, comment, created_on)
+VALUES (5678, 9012, 1234, 'Its a beautiful day in the neighborhood', '2004-10-19 10:23:54')
 
+INSERT INTO posts (id, tweet, stamp, user_id)
+VALUES (9012, 'What is today?', '2004-10-19 07:21:51', 1234)
